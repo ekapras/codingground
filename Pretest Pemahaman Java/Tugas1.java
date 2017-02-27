54a2564a58e2e2ebfbaf262b7f55490c6427ea25
ekapras <ekapras19@gmail.com>
2017-02-27 11:00:09 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas1.java
@@ -0,0 +1,8 @@
+public class Tugas1{
+
+     public static void main(String []args){
+        String nama = "M.Eka Prasetyo";
+        String kota = "Jepara";
+        System.out.println(nama + " " + kota);
+     }
+}
